90b2f60ae9dd9708b1b37275abb978dbe3d80802
ALLBOTR <126163652+ALLBOTR@users.noreply.github.com>
2023-04-04 07:31:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'SI' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://www.linkpicture.com/q/aniyuki-cute-anime-avatar-profile-picture-10-768x768.jpg' : process.env.ALIVE_LOGO,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/Nezuko/6th.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false': process.env.ONLY_GROUP,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true': process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '💟☻️ඉන්බොක්ස් වැඩ නැ බන්☻️💟' : process.env.INBOX_BL_MSG,
-HOST: process.env.HOST === undefined ? '*HEROKU*' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=onQyxTDa#-WHmGpI1a07hkk-gLIOf_0GSoNHlF2uIPbKM2htxFEQ' : process.env.SESSION_ID
+HOST: process.env.HOST === undefined ? 'HEROKU' : process.env.HOST,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=oiQAQK7Z#4uV-e7GFMVtLUjEOKXivMlMp7YmzjaW-ZXYa3Bqas4Q' : process.env.SESSION_ID
 };
